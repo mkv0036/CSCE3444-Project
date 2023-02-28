2907f515f7cf1d5f5eb5c63e51b7c25980335f0e
madeline <72455258+mkv0036@users.noreply.github.com>
2023-02-28 03:35:00 +0000

    uploading work from replit

---
+++ script.js
@@ -0,0 +1,20 @@
+const searchForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.search-result');
+const container = document.querySelector('.container');
+let searchQuery = '';
+const APP_ID = 'a0701856';
+const APP_KEY = 'dd1f112182912aeaf4f5de60f8adac62';
+
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector('input').value;
+  fetchAPI();
+});
+
+async function fetchAPI(){
+  const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=a0701856&app_key=dd1f112182912aeaf4f5de60f8adac62
+&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const response = await fetch(baseURL);
+  console.log(response);
+}
